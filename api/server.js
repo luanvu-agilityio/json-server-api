6a0192e73f8e70f866503385d3aa1774c8fad433
Luan Vu <116854422+luanvvk@users.noreply.github.com>
2025-01-15 21:50:47 +0700

    test

--- api/server.js
+++ api/server.js
@@ -1,26 +1,23 @@
-const path = require('path');
 const jsonServer = require('json-server');
-const cors = require('cors');
 const server = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, 'db.json'));
+const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
-// Enable CORS for all origins
-server.use(
-  cors({
-    origin: true,
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-  }),
-);
+
 
 server.use(middlewares);
+
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', '*');
+  res.header('Access-Control-Allow-Methods', '*');
+  if (req.method === 'OPTIONS') {
+    return res.send(200);
+  }
+  next();
+});
 server.use(router);
 
 
-server.listen(port, () => {
-  console.log(`JSON Server is running on port ${port}`);
-});
 
 module.exports = server;
