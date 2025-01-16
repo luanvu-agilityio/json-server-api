9afb68b13bd790d6741e2c70c6fa0bd96915a303
Luan Vu <luan.vu@asnet.com.vn>
2025-01-16 09:18:29 +0700

    test

--- api/server.js
+++ api/server.js
@@ -5,13 +5,13 @@ const middlewares = jsonServer.defaults();
 
 // Enable CORS for all requests
 server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
   res.header(
     'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
   );
-  res.header('Access-Control-Max-Age', '86400');
 
   // Handle preflight requests
   if (req.method === 'OPTIONS') {
