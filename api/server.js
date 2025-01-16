30b47f761a357ff74117077ad7d07fbc510c5010
Luan Vu <luan.vu@asnet.com.vn>
2025-01-16 08:38:56 +0700

    update code

--- api/server.js
+++ api/server.js
@@ -3,17 +3,25 @@ const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
-server.use(middlewares);
-
+// Enable CORS for all requests
 server.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', '*');
-  res.header('Access-Control-Allow-Methods', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+  );
+  res.header('Access-Control-Max-Age', '86400');
+
+  // Handle preflight requests
   if (req.method === 'OPTIONS') {
-    return res.sendStatus(200);
+    res.sendStatus(200);
+    return;
   }
   next();
 });
+
+server.use(middlewares);
 server.use(router);
 
 module.exports = server;
