e3287e7bc4110d242aec4fb2316b962b43e38ccb
Luan Vu <luan.vu@asnet.com.vn>
2025-01-21 10:04:16 +0700

    update to test

--- api/index.js
+++ api/index.js
@@ -1,13 +1,11 @@
-const fs = require('fs');
-const path = require('path');
-const db = JSON.parse(fs.readFileSync(path.join('db.json')));
-
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router(db, {
-  preserveOrder: true,
-});
 const middlewares = jsonServer.defaults();
+
+// Create a memory adapter that will act as our database
+const memory = require('json-server/lib/server/router/memory');
+const db = require('./db.json');
+const router = jsonServer.router(memory.create(db));
 
 // Enable CORS for all requests
 server.use((req, res, next) => {
@@ -27,7 +25,17 @@ server.use((req, res, next) => {
   next();
 });
 
+// Add custom middleware to log database state
+server.use((req, res, next) => {
+  console.log('Current DB State:', router.db.getState());
+  next();
+});
+
 server.use(middlewares);
 server.use(router);
 
-module.exports = server;
+// Add error handling
+server.use((err, req, res, next) => {
+  console.error('Server Error:', err);
+  res.status(500).json({ error: 'Internal Server Error', details: err.message });
+});
