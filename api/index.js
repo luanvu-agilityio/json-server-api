e159462fe5dfabd9f8f6183b97162f56de3e6d4f
Luan Vu <luan.vu@asnet.com.vn>
2025-01-21 14:17:02 +0700

    test8: retest

--- api/index.js
+++ api/index.js
@@ -2,7 +2,6 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 
 const fs = require('fs');
-const { json } = require('stream/consumers');
 const path = requite('path');
 const filePath = path.join('db.json');
 const data = fs.readFileSync(filePath, 'utf-8');
@@ -11,23 +10,23 @@ const router = jsonServer.router(db);
 
 const middlewares = jsonServer.defaults();
 
-// Enable CORS for all requests
-server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
-  );
+// // Enable CORS for all requests
+// server.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Credentials', 'true');
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+//   res.header(
+//     'Access-Control-Allow-Headers',
+//     'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+//   );
 
-  // Handle preflight requests
-  if (req.method === 'OPTIONS') {
-    res.sendStatus(200);
-    return;
-  }
-  next();
-});
+//   // Handle preflight requests
+//   if (req.method === 'OPTIONS') {
+//     res.sendStatus(200);
+//     return;
+//   }
+//   next();
+// });
 
 server.use(middlewares);
 server.use(
